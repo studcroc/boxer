8ea35d430a8b0381aa30a809bf0ca54f2a1bd5ce
Harsh Vardhan Gautam <hvgautam24.vnit@gmail.com>
2021-09-07 10:54:45 +0530

    UPDATE default log label ADD example app

--- lib/boxer.dart
+++ lib/boxer.dart
@@ -5,16 +5,16 @@ import 'dart:developer';
 import 'package:intl/intl.dart';
 
 class Boxer {
-  static punch(String message, {String? label}) {
+  static punch(dynamic message, {String? label}) {
     String s = StackTrace.current.toString();
     var list = s.split("\n");
     var sourcePath = list[1].substring(list[1].indexOf("package"));
     DateTime now = DateTime.now();
     final DateFormat formatter = DateFormat('yyyy-MM-dd | hh:mm:ss');
     final String dt = formatter.format(now);
-    log(
-      "\n$dt $message $sourcePath",
-      name: label ?? "DEBUG",
-    );
+    label = label ??
+        sourcePath.substring(
+            sourcePath.indexOf("/") + 1, sourcePath.lastIndexOf(":"));
+    log("\n $dt ${message.toString()} $sourcePath", name: label);
   }
 }
