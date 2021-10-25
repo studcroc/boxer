ca3c60d6cf0a145b1f849cffcb7313cf02e6b351
Harsh Vardhan Gautam <hvgautam24.vnit@gmail.com>
2021-10-25 17:53:22 +0530

    Update sourcePath extracting logic

--- lib/boxer.dart
+++ lib/boxer.dart
@@ -20,28 +20,30 @@ class Boxer {
   /// [_printLogs] method is responsible for printing the log messages passed to it as [vargs] of type [List]
   static void _printLogs(
       List<dynamic> vargs, String? label, StackTrace current) {
-    String s = current.toString();
-    var list = s.split('\n');
-    var sourcePath = list[1].substring(list[1].indexOf('package'));
+    var sourcePath = "";
+    var regex = RegExp(r"[a-zA-Z]*:.*[a-zA-Z]:{1}\d\d", multiLine: false);
+    var matches = regex.allMatches(current.toString());
+    if (matches.isNotEmpty && matches.length >= 2) {
+      int start = matches.elementAt(0).start;
+      int end = matches.elementAt(0).end;
+      sourcePath = current.toString().substring(start, end);
+    }
     DateTime now = DateTime.now();
     final DateFormat formatter = DateFormat('yyyy-MM-dd | hh:mm:ss');
     final String dt = formatter.format(now);
     label = label ??
         sourcePath.substring(
             sourcePath.indexOf('/') + 1, sourcePath.lastIndexOf(':'));
-    log('$yellow$dt $sourcePath', name: label);
+    log('$yellow$dt || $sourcePath', name: label);
     for (var item in vargs) {
       if (item is Exception) {
         dynamic x = item;
-        log('$white$redbg${x.message}', name: label, stackTrace: current);
-        log('$white$redbg${x.source}', name: label, stackTrace: current);
+        log('$white$redbg${x.message}', name: label, error: current);
       } else if (item is Error) {
         dynamic x = item;
-        log('$white$redbg${x.toString()}', name: label, stackTrace: current);
-        log('$white$redbg${x.stackTrace.toString()}',
-            name: label, stackTrace: current);
+        log('$white$redbg${x.toString()}', name: label, error: current);
       } else {
-        log('$white$item', name: label, stackTrace: current);
+        log('${item.toString().replaceAll("", white)}', name: label);
       }
     }
   }
